85d0f5d24fad01973b34b3c570f8b92261d58165
Mauricio Matsuda <mauricio@webgoal.com.br>
2017-04-03 11:55:35 -0300

    Add moip template

---
+++ templates/moip/Templates.js
@@ -0,0 +1,24 @@
+[
+  {
+    app_source: 'moip',
+    app_target: 'granatum_financeiro',
+    filters: [[{field: "status", condition: "equal", value: "PAID"}, {field: ["payments", 0, "status"], condition: "not_equal", value: "SETTLED"}]],
+    mapper: [
+    // include mapping
+    ],
+    trigger_key: 'new_transactions',
+    action_key: 'add_recebimento',
+    name: {pt_BR: 'A cada pagamento aprovado na Moip incluir um recebimento no Granatum Financeiro',
+          en: 'For every approved payment on Moip, generate an accounts receivable on Granatum Financeiro'},
+    description: {
+      why_integrate: [{pt_BR: 'O objetivo é ter o seu fluxo de caixa do Granatum sempre atualizado, sem precisar verificar e atualizar manualmente quem pagou e quem não pagou. ;)',
+                      en: 'The goal is to have your Granatum cash flow always up to date without having to manually check and update who paid and who did not pay.'}],
+      how_it_works: [{pt_BR: 'Pluga fica de olho em cada novo pagamento aprovado na sua conta da Moip.',
+                      en: 'Pluga keeps an eye on every new approved payment on your Moip account.'},
+                      {pt_BR: 'Quando houver um novo pagamento aprovado, nós incluiremos um recebimento no Granatum Financeiro.',
+                      en: 'When a new payment is approved, we will include a receipt in the Granatum Financeiro.'},
+                      {pt_BR: 'Este dado será incluído automaticamente no Granatum e ficará disponível na lista de lançamentos.',
+                      en: 'This data will be automatically included in the Granatum and will be available in the list of "lançamentos".'}]
+    }
+  }
+]
