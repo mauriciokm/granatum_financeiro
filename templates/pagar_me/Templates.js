dd986225500642d3d9b8ed433cb92b50eea3fc71
Mauricio Matsuda <mauricio@webgoal.com.br>
2017-04-03 11:55:10 -0300

    Fix pagar_me template

--- templates/pagar_me/Templates.js
+++ templates/pagar_me/Templates.js
@@ -1,6 +1,6 @@
 [
   {
-    app_source: 'pag_seguro',
+    app_source: 'pagar_me',
     app_target: 'granatum_financeiro',
     filters: [[{field: 'status', condition: 'equal', value: 'paid'}]],
     mapper: [
@@ -8,17 +8,17 @@
     ],
     trigger_key: 'new_transactions',
     action_key: 'add_recebimento',
-    name: {pt_BR: 'Para cada fatura gerada no Pagar.me incluir uma conta a receber no Granatum Financeiro',
-          en: 'For each invoice generated in Pagar.me includes an account receivable in Granatum Financeiro'},
+    name: {pt_BR: 'A cada pagamento aprovado na Pagar.me incluir um recebimento no Granatum Financeiro',
+          en: 'For every approved payment on Pagar.me, generate an accounts receivable on Granatum Financeiro'},
     description: {
-      why_integrate: [{pt_BR: 'O objetivo é ter o seu fluxo de caixa do Granatum sempre atualizado, sem precisar verificar e atualizar manualmente as contas a receber. ;)',
-                      en: 'The goal is to have your Granatum cash flow always up to date without having to manually check and update accounts receivable.'}],
-      how_it_works: [{pt_BR: ' Pluga fica de olho em cada nova fatura na sua conta da Pagar.me.',
-                      en: 'Pluga keep an eye on each new invoice in your Pagar.me account.'},
-                      {pt_BR: 'Quando uma nova fatura for criada na Pagar.me, nós incluiremos uma conta a receber no Granatum Financeiro.',
-                      en: 'When a new invoice is created in Pagar.me, we will include an account receivable in the Granatum Financeiro'},
+      why_integrate: [{pt_BR: 'O objetivo é ter o seu fluxo de caixa do Granatum sempre atualizado, sem precisar verificar e atualizar manualmente quem pagou e quem não pagou. ;)',
+                      en: 'The goal is to have your Granatum cash flow always up to date without having to manually check and update who paid and who did not pay.'}],
+      how_it_works: [{pt_BR: ' Pluga fica de olho em cada novo pagamento aprovado na sua conta da Pagar.me.',
+                      en: 'Pluga keeps an eye on every new approved payment on your Pagar.me account.'},
+                      {pt_BR: 'Quando houver um novo pagamento aprovado, nós incluiremos um recebimento no Granatum Financeiro.',
+                      en: 'When a new payment is approved, we will include a receipt in the Granatum Financeiro.'},
                       {pt_BR: 'Este dado será incluído automaticamente no Granatum e ficará disponível na lista de lançamentos.',
-                      en: 'This data will be automatically included in the Granatum and will be available in the list of "lançamentos"'}]
+                      en: 'This data will be automatically included in the Granatum and will be available in the list of "lançamentos".'}]
     }
   }
 ]
